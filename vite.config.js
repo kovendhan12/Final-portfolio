3afc7f0f190197247ae07d884a26c87ad02cc4c0
kovendhan <kovendhan64@gmail.com>
2025-12-12 21:28:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base:'/Final-portfolio/',
-  resolve: {
+  resolve: { 
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
