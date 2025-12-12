6cd22029b3222c097e395411f4cb19051c2969fe
kovendhan <kovendhan64@gmail.com>
2025-12-12 21:19:55 +0530

    Fix formatting of base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/Final-portfolio/',
+  base: '/Final-portfolio/',  // <-- IMPORTANT (edit this only)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
